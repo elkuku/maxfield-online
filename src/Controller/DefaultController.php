6b5ba5544b04b62cf9664cdc34d3fa996be858ea
Nikolai Plath <der.el.kuku@gmail.com>
2022-04-02 08:51:28 -0500

    Testing...

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,22 +2,44 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends BaseController
 {
     #[Route('/', name: 'default', methods: ['GET'])]
     public function index(string $projectDir): Response {
+        $fileSystem = new Filesystem();
+
+        $projectRoot = $projectDir.'/public/maxfields/aaatest';
+
+        if (is_dir($projectRoot)) {
+            $this->addFlash('success', 'dir exists: '.$projectRoot);
+        } else {
+            $this->addFlash('danger', 'dir does not exists: '.$projectRoot);
+            $fileSystem->mkdir($projectRoot);
+        }
+
+        if (is_dir($projectRoot)) {
+            $this->addFlash('success', 'dir exists: '.$projectRoot);
+        } else {
+            throw new \UnexpectedValueException(
+                'There was a problem creating your Project directory: '
+                .$projectRoot
+            );
+        }
+
+        $fileName = $projectRoot.'/portals.txt';
+        $fileSystem->dumpFile($fileName, 'Hello Dolly :)');
+
+        $hello = file_get_contents($fileName);
+
         return $this->render(
             'default/index.html.twig',
             [
                 'maxfields' => $this->getUser()?->getMaxfields(),
-                'controller_name' => 'DefaultController',
-                'php_version'     => PHP_VERSION,
-                'symfony_version' => Kernel::VERSION,
-                'project_dir'     => $projectDir,
+                'hello' => $hello,
             ]
         );
     }
